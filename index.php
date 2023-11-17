0328a13ada248a809289949b2feb5b4ed3176d59
indiefoxart <94010300+indiefoxart@users.noreply.github.com>
2023-11-17 08:26:01 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <title>fwhip.xyz</title>
+    <meta property="og:title" content="fwhip.xyz">
+    <meta name="twitter:title" content="fwhip.xyz">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="washing_machine">
+        <div class="door"></div>
+        <div class="water"></div>
+        <img class="image" src="fwhip.png" style="height: 185px;">
+    </div>
+    <div class="tip">
+        <h2>get washing machined. idiot.</h2>
+    </div>
+    <p>fox@empiressmp.gay</p>
+</body>
+</html>
+
